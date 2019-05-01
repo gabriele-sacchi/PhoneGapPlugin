bf3d92eaf0cd600fe56ad6f3219ace50923fc9eb
gabriele-sacchi <48427520+gabriele-sacchi@users.noreply.github.com>
2019-05-01 16:15:13 -0400

    Update marketo.js

--- www/marketo.js
+++ www/marketo.js
@@ -1,4 +1,4 @@
-window.isForkedMarketo = true;
+window.hasForkedMarketo = true;
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
